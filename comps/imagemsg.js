cbfb43c1002c71adb67a2f65f84117767b84173a
nwmaker <makernw@gmail.com>
2018-02-11 21:52:00 -0700

    add-fades

--- comps/imagemsg.js
+++ comps/imagemsg.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default ({ name, message }) => {
   const image = `../static/${name}.png`
   return (
-    <div>
+    <div className='image-msg'>
       <img className={name} src={image} alt=''/>
       <div className='msg'>
         {message}
---
+++ pages/fades.js
@@ -0,0 +1,83 @@
+import React from 'react'
+import ImageMsg from '../comps/imagemsg'
+
+const images = [
+  'monarch',
+  'mountain',
+  'rocks',
+  'fish'
+]
+
+const messages = [
+  'experience',
+  'memory',
+  'exploration',
+  'moment'
+]
+
+export default () => {
+  let slides = images.map(
+    (image, index) => <ImageMsg name={image} message={messages[index]}/>
+  )
+  return (
+    <div className='show'>
+      { slides }
+      <style jsx global>{`
+        .show {
+          height:360px;
+          width:800px;
+          position: relative;
+          margin: 0 auto;
+        }
+        .msg {
+          position: absolute;
+          color: white;
+          font-size: 30px;
+          bottom: 10px;
+          left: 15px;
+          font-family: 'Josefin Slab', serif;
+          font-weight: 100;
+        }
+        .show .image-msg {
+          position: absolute;
+          left: 0;
+        }
+        .image-msg {
+          animation-name: fadeInOut;
+          animation-timing-function: ease-in-out;
+          animation-iteration-count: infinite;
+          animation-duration: 8s;
+        }
+        @keyframes fadeInOut {
+          0% {
+            opacity: 1;
+	  }
+          10% {
+            opacity: 1;
+          }
+	  25% {
+            opacity: 0;
+	  }
+          90% {
+	    opacity: 0;
+	  }
+          100% {
+            opacity: 1;
+          }
+	}
+        .image-msg:nth-of-type(1) {
+          animation-delay: 6s;
+        }
+        .image-msg:nth-of-type(2) {
+          animation-delay: 4s;
+        }
+        .image-msg:nth-of-type(3) {
+          animation-delay: 2s;
+        }
+        .image-msg:nth-of-type(4) {
+          animation-delay: 0;
+        }
+      `}</style>
+    </div>
+  )
+}
